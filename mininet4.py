4924f824694867b8a9dc8896028f6a87eeeed1ab
jrojas <93329949+100472823@users.noreply.github.com>
2024-04-05 11:30:24 +0200

    Update mininet4.py

--- mininet4.py
+++ mininet4.py
@@ -7,21 +7,33 @@ from mininet.node import RemoteController, OVSSwitch
 from functools import partial
 
 
-class SingleSwitchTopo(Topo):
+class Hito1Topo(Topo):
     'My single switch connected to n hosts.'
 
     def build(self, N=2):
-        switch = self.addSwitch('s1')
-        for hn in range(N):
-            mac_suffix = format(hn + 1, '02x')
-            mac_address = f'00:00:00:00:00:{mac_suffix}'
-            host = self.addHost(f'h{hn+1}', ip='1.1.1.1', mac=mac_address)
-            self.addLink(host, switch)
+        switch = self.addSwitch('s1', mac='70:88:99:00:00:01', ip='10.0.0.1/24')
+        host1 = self.addHost('h1', ip='10.0.0.2/24', mac='00:00:00:00:00:01')
+        host2 = self.addHost('h2', ip='10.0.1.2/24', mac='00:00:00:00:00:02')
+        
+        self.addLink(host1, switch, intfName2='s1-eth1')
+        self.addLink(host2, switch, intfName2='s1-eth2')
+
+def configureStaticARP(host, ip, mac):
+    # Establecer una entrada ARP estática en el host
+    host.setARP(ip=ip, mac=mac)
+
+
+def configureStaticARPs(net):
+    # Configurar entradas ARP estáticas en ambos hosts
+    configureStaticARP(net.get('h1'), ip='10.0.1.2', mac='70:88:99:00:00:01')
+    configureStaticARP(net.get('h2'), ip='10.0.0.1', mac='70:88:99:00:00:01')
+
+
 
 
 def simpleTestCLI():
-    topo = SingleSwitchTopo(4)
-    net = Mininet(topo,controller=partial(RemoteController, ip="127.0.0.1"),switch=partial(OVSSwitch, protocols="OpenFlow1"))
+    topo = Hito1Topo(4)
+    net = Mininet(topo,controller=partial(RemoteController, ip="127.0.0.1"),switch=partial(OVSSwitch, protocols="OpenFlow13"))
     net.start()
     CLI(net)
     net.stop()
